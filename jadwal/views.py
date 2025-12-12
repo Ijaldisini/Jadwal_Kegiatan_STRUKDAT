594b052d46769fd9051d796d016dc797bfc3cfde
iZall. <radityafd07@gmail.com>
2025-12-12 18:04:57 +0700

    fix view

--- jadwal/views.py
+++ jadwal/views.py
@@ -2,23 +2,36 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 from .models import Kegiatan
 from .forms import KegiatanForm
+from django.core.paginator import Paginator
 
 
 # Menggantikan show_all_controller.py
 def daftar_kegiatan(request):
-    query = request.GET.get("q")  # Untuk fitur cari (search_controller.py)
-    if query:
-        kegiatan_list = Kegiatan.objects.filter(
-            Q(nama__icontains=query)
-            | Q(tempat__icontains=query)
-            | Q(tanggal__icontains=query)
-        )
-    else:
-        kegiatan_list = Kegiatan.objects.all()
+    query = request.GET.get('q', '')
+    sort = request.GET.get('sort', 'asc')  # Default urutan terlama (asc)
 
-    return render(
-        request, "jadwal/index.html", {"kegiatan_list": kegiatan_list, "query": query}
-    )
+    # 1. Ambil data & Filter Pencarian
+    if query:
+        semua_kegiatan = Kegiatan.objects.filter(nama__icontains=query)
+    else:
+        semua_kegiatan = Kegiatan.objects.all()
+
+    # 2. Logika Pengurutan
+    if sort == 'desc':
+        semua_kegiatan = semua_kegiatan.order_by('-tanggal', '-waktu_mulai')
+    else:
+        semua_kegiatan = semua_kegiatan.order_by('tanggal', 'waktu_mulai')
+
+    # 3. Paginasi (10 data)
+    paginator = Paginator(semua_kegiatan, 10)
+    page_number = request.GET.get('page')
+    kegiatan_list = paginator.get_page(page_number)
+
+    return render(request, 'jadwal/index.html', {
+        'kegiatan_list': kegiatan_list,
+        'query': query,
+        'sort': sort,
+    })
 
 
 # Menggantikan add_controller.py
